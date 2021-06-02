d67d989d3deae5b54dffbff8e1a6aa1bd1f3ee75
theinfinox <1sreegovind@gmail.com>
2021-06-02 12:30:28 +0530

    Create sum_of_two_num.c

---
+++ code/sum_of_two_num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int a,b;
+    printf("Enter the number a :");
+    scanf("%d",&a);
+    printf("Enter the number b :");
+    scanf("%d",&b);
+    int c = a+b;
+    printf("%d",c);
+}
