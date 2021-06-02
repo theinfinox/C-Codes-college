58160e0d762d32b97a46476c7135362684ac8ad3
theinfinox <1sreegovind@gmail.com>
2021-06-02 11:52:11 +0530

    Update intprint.c

--- code/intprint.c
+++ code/intprint.c
@@ -5,5 +5,15 @@ void main(){
     printf("value of a is %d \n",a);
     printf("value of a is %f \n",b);
 
+    //char %c
+    //float %f
+    //int %d
+    //long int %lu
+    //double %lf
+    //string %s
+    int c;
+    printf("enter the value of c:");
+    scanf("%d",&c);
+    printf("the value of c is %d",c);
 
 }
