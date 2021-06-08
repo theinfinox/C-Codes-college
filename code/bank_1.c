9de41522c2258c3e4e12784f09708458ee8a571f
theinfinox <1sreegovind@gmail.com>
2021-06-08 12:15:55 +0530

    Create bank_1.c

---
+++ code/bank_1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+  char g;
+  float b,n;
+  printf("Enter your gender: ");
+  scanf("%c",&g);
+  printf("Enter your balance: ");
+  scanf("%f",&b);
+  if(g=='f' || g=='F'){
+    if(b>5000){
+      n=0.05*b;
+      printf("Your bonus is %f\n",n);
+    }
+    else{
+      n=0.02*b;
+      printf("Your bonus is %f\n",n);
+    }
+  }
+  else if(g=='m' || g=='M'){
+    n=0.02*b;
+    printf("Your bonus is %f\n",n);
+  }
+  else{
+    printf("incorrect format\n");
+  }
+}
